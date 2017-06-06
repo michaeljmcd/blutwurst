9fb95815e0042cc2f0c19f8d21aef8ed587199a4
Michael McDermott <mmcdermott@mad-computer-scientist.com>
2017-06-06 10:29:33 -0500

    Patched table creation code, in part with a fixed connection string; Also removed unneeded record type.

--- src/blutwurst/database.clj
+++ src/blutwurst/database.clj
@@ -2,11 +2,19 @@
     (:import (java.sql DriverManager)))
 
 (defrecord schema-graph [tables dependencies])
-(defrecord database-spec [connection-string])
+
+(defn retrieve-tables [spec]
+ (let [connection (DriverManager/getConnection (:connection-string spec))
+       meta-data (.getMetaData connection)
+       result-set (.getTables meta-data nil nil nil (into-array ["TABLE"]))]
+   (while (.next result-set)
+     (println "on row")
+     (println (.getString result-set "TABLE_NAME"))
+     )
+ ))
 
 (defn retrieve-table-graph [spec]
  "This function accepts a connection specification and produces a table graph."
- (let [connection (DriverManager/getConnection (:connection-string spec))
-       meta-data (.getMetaData connection)]
- nil
- ))
+ (let [tables (retrieve-tables spec)]
+   nil
+   ))
--- test/blutwurst/database_test.clj
+++ test/blutwurst/database_test.clj
@@ -3,17 +3,21 @@
             [blutwurst.database :refer :all])
     (:import (java.sql DriverManager)))
 
-(defn create-test-tables []
- (let [connection (DriverManager/getConnection "jdbc:h2:mem:")
-       statement (.createStatement connection)
-       table-creation-sql ["create schema dbo" "create table dbo.Person (ID int, Name varchar(100))"]]
-    (for [i table-creation-sql]  (.execute statement i))
- ))
+(def connection-string "jdbc:h2:mem:test")
+
+(defn create-test-tables [connection-string] 
+  (let [connection (DriverManager/getConnection connection-string)
+        statement (.createStatement connection)
+        table-creation-sql ["create schema dbo" "create table dbo.Person (ID int, Name varchar(100))"]]
+    (doseq [i table-creation-sql]
+      (.execute statement i))
+    (.close statement)
+  ))
 
 (deftest table-graph-tests
   (testing "Connects to an in-memory database and returns null."
-   (let [spec (->database-spec "jdbc:h2:mem:")]
-       (create-test-tables)
+   (let [spec {:connection-string connection-string}]
+       (create-test-tables connection-string)
        (let [table-graph (retrieve-table-graph spec)]
     (is (not (= table-graph nil)))
    )
