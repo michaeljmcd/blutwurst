a27145302ed0a113f16400d2fb5f6f27357a003d
Michael McDermott <mmcdermott@mad-computer-scientist.com>
2017-06-06 17:02:50 -0500

    Modified the tests to use fixtures.

--- test/blutwurst/database_test.clj
+++ test/blutwurst/database_test.clj
@@ -5,6 +5,13 @@
 
 (def connection-string "jdbc:h2:mem:test")
 
+(defn clear-database [connection-string]
+  (let [connection (DriverManager/getConnection connection-string)
+        statement (.createStatement connection)]
+    (.execute statement "drop all objects")
+    (.close statement)
+  ))
+
 (defn create-test-tables [connection-string] 
   (let [connection (DriverManager/getConnection connection-string)
         statement (.createStatement connection)
@@ -14,13 +21,18 @@
     (.close statement)
   ))
 
+(defn db-fixture [f]
+  (create-test-tables connection-string)
+  (f)
+  (clear-database connection-string))
+
+(use-fixtures :each db-fixture)
+
 (deftest table-graph-tests
   (testing "Connects to an in-memory database and returns basic table list."
-   (let [spec {:connection-string connection-string}]
-       (create-test-tables connection-string)
-     (let [table-graph (retrieve-table-graph spec)]
+     (let [spec {:connection-string connection-string}
+           table-graph (retrieve-table-graph spec)]
        (println table-graph)
        (is (= {:tables '({:name "PERSON" :schema "DBO" :columns ({:name "NAME"} {:name "ID"})})}
-              table-graph))
-       ))
+              table-graph)))
    ))
