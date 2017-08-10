2b3123a1c825b1deea1b6f505219acb07bfde1f4
Michael McDermott <mcdermott.michaelj@gmail.com>
2017-08-10 16:00:11 -0500

    Added simple foreign key example as a failing test.

--- test/blutwurst/planner_test.clj
+++ test/blutwurst/planner_test.clj
@@ -4,16 +4,30 @@
 
 (deftest planner-tests
   (testing "Testing the process of sequencing tables."
-    (let [spec nil
-          simple-schema {:tables '({:name "PERSON",
+    (let [simple-schema {:tables '({:name "PERSON",
                          :schema "DBO",
                          :columns
-                         ({:name "NAME", :type "VARCHAR", :length 100, :nullable true}
-                          {:name "ID", :type "INTEGER", :length 10, :nullable true})})}]
+                         [{:name "NAME", :type "VARCHAR", :length 100, :nullable true}
+                          {:name "ID", :type "INTEGER", :length 10, :nullable true}]})}]
      (is (= '({:name "PERSON",
                          :schema "DBO",
                          :columns
-                         ({:name "NAME", :type "VARCHAR", :length 100, :nullable true}
-                          {:name "ID", :type "INTEGER", :length 10, :nullable true})})
+                         [{:name "NAME", :type "VARCHAR", :length 100, :nullable true}
+                          {:name "ID", :type "INTEGER", :length 10, :nullable true}]})
           (create-data-generation-plan simple-schema)))
-    )))
+    ))
+
+  (testing "Planning should account for foreign key relationships when buidling out a plan."
+   (let [city-table-definition {:name "CITY" :schema "ASDF" :columns '({:name "STATE"})
+                                        :dependencies [
+                                          { :dependency-name "D1" :target-name "STATE"
+                                            :target-schema "ASDF" :links {"STATE" "NAME"}}
+                                        ]}
+         state-table-definition {:name "STATE" :schema "ASDF" :columns '({:name "NAME"})
+                                        :dependencies []}
+         foreign-key-schema {:tables (list city-table-definition state-table-definition)}]
+     (is (= (list state-table-definition city-table-definition)
+            (create-data-generation-plan foreign-key-schema)
+         )))
+                                       
+  ))
