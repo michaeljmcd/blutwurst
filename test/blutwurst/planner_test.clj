a09a4f687be79c49c1fc72d82e6ea5552e631042
Michael McDermott <mcdermott.michaelj@gmail.com>
2017-08-11 12:25:01 -0500

    Created a test case that breaks the current sort.

--- test/blutwurst/planner_test.clj
+++ test/blutwurst/planner_test.clj
@@ -1,6 +1,7 @@
 (ns blutwurst.planner-test
   (:require [clojure.test :refer :all]
             [blutwurst.logging-fixture :refer :all]
+            [taoensso.timbre :as timbre :refer [trace]]
             [blutwurst.planner :refer :all]))
 
 (use-fixtures :each logging-fixture)
@@ -19,16 +20,22 @@
     ))
 
   (testing "Planning should account for foreign key relationships when buidling out a plan."
-   (let [city-table-definition {:name "CITY" :schema "ASDF" :columns '({:name "STATE"})
+   (let [city-table-definition {:name "CITY" :schema "ASDF" :columns '({:name "STATE"} {:name "COUNTRY"} {:name "NAME"})
                                         :dependencies [
                                           { :dependency-name "D1" :target-name "STATE"
                                             :target-schema "ASDF" :links {"STATE" "NAME"}}
+                                          { :dependency-name "D2" :target-name "COUNTRY"
+                                            :target-schema "ASDF" :links {"COUNTRY" "NAME"}}
                                         ]}
+         country-table-definition {:name "COUNTRY" :schema "ASDF" :columns '({:name "NAME"})}
          state-table-definition {:name "STATE" :schema "ASDF" :columns '({:name "NAME"})
                                         :dependencies []}
-         foreign-key-schema {:tables (list city-table-definition state-table-definition)}]
+         foreign-key-schema {:tables (list country-table-definition city-table-definition state-table-definition)}
+         result (create-data-generation-plan foreign-key-schema)]
 
-     (is (= (list state-table-definition city-table-definition)
-            (create-data-generation-plan foreign-key-schema)
+     (trace "Table order: " (pr-str (seq (map #(str (:schema %) "." (:name %)) result))))
+
+     (is (= (list country-table-definition state-table-definition city-table-definition)
+            result
          )))
   ))
